cf1d01e6e9cb4f218086c1107982d17e5ef03070
Daniel <daniel.albert.liu@gmail.com>
2024-10-11 15:57:18 -0500

    changed separators

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -9,7 +9,7 @@ return {
 			require("lualine").setup({
 				options = {
 					component_separators = { left = "│", right = "│" },
-					section_separators = { left = "▌", right = "▐" },
+					section_separators = { left = "", right = "" },
 				},
 			})
 		end,
