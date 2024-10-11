e0f65e1df35335e08d4ebef40de20622ce666d7a
Daniel <daniel.albert.liu@gmail.com>
2024-10-11 15:53:10 -0500

    stuff

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -6,7 +6,12 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
-			require("lualine").setup({})
+			require("lualine").setup({
+				options = {
+					component_separators = { left = "│", right = "│" },
+					section_separators = { left = "▌", right = "▐" },
+				},
+			})
 		end,
 	},
 }
