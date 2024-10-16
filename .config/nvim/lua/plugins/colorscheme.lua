924071817817d64532b69a64c57b1fbed117afee
Daniel <daniel.albert.liu@gmail.com>
2024-10-15 21:47:58 -0500

    dracula

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,28 @@
 return {
 	-- Dracula
 	{
-		"Mofiqul/dracula.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+		"dracula/vim",
+		priority = 1000,
 		init = function()
-			-- Load the colorscheme here.
-			-- vim.cmd.colorscheme("dracula")
+			vim.cmd.colorscheme("dracula")
 		end,
 	},
 
 	-- One Dark Pro
 	{
 		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+		priority = 1000,
 		init = function()
-			-- Load the colorscheme here.
-			vim.cmd.colorscheme("onedark")
+			-- vim.cmd.colorscheme("onedark")
 		end,
 		config = function()
 			local colors = require("onedarkpro.helpers").get_colors()
 			require("onedarkpro").setup({
 				highlights = {
-					["@comment"] = { fg = colors.comment, italic = true },
 					CursorLineNr = { fg = colors.white, extend = true },
+				},
+				styles = {
+					comments = "italic",
 				},
 				options = {
 					cursorline = true,
