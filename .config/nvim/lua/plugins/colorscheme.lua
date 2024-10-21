d161e2a68f05adc999ecf1df4899f6b77aef5263
Daniel <daniel.albert.liu@gmail.com>
2024-10-21 13:14:53 -0500

    dracula soft

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,27 @@
 return {
 	-- Dracula
 	{
-		"dracula/vim",
+		"Mofiqul/dracula.nvim",
 		priority = 1000,
 		init = function()
-			vim.cmd.colorscheme("dracula")
+			vim.cmd.colorscheme("dracula-soft")
+		end,
+		config = function()
+			require("dracula").setup({
+				colors = {
+					selection = "#44475A",
+				},
+				overrides = function(colors)
+					return {
+						CmpItemAbbr = { fg = colors.fg, bg = colors.menu },
+						CmpItemKind = { fg = colors.fg, bg = colors.menu },
+						NormalFloat = { fg = colors.fg, bg = colors.menu },
+					}
+				end,
+				italic_comment = false,
+				transparent_bg = false,
+				show_end_of_buffer = true,
+			})
 		end,
 	},
 
@@ -19,10 +36,8 @@ return {
 			local colors = require("onedarkpro.helpers").get_colors()
 			require("onedarkpro").setup({
 				highlights = {
+					["@comment"] = { fg = colors.comment, italic = true },
 					CursorLineNr = { fg = colors.white, extend = true },
-				},
-				styles = {
-					comments = "italic",
 				},
 				options = {
 					cursorline = true,
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -6,7 +6,40 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
-			require("lualine").setup({})
+			local custom_dracula = require("lualine.themes.dracula")
+			local colors = require("dracula").colors()
+			local modes = {
+				normal = colors.purple,
+				insert = colors.green,
+				visual = colors.yellow,
+				replace = colors.red,
+				command = colors.orange,
+				inactive = colors.selection,
+			}
+			for mode, color in pairs(modes) do
+				custom_dracula[mode].a.fg = colors.black
+				custom_dracula[mode].a.bg = color
+				custom_dracula[mode].b.fg = colors.fg
+				custom_dracula[mode].b.bg = colors.bg
+				custom_dracula[mode].c.fg = colors.fg
+				custom_dracula[mode].c.bg = colors.bg
+			end
+
+			require("lualine").setup({
+				options = {
+					theme = custom_dracula,
+					component_separators = "|",
+					section_separators = "",
+				},
+				sections = {
+					lualine_a = { { "mode", separator = { left = "", right = "" } } },
+					lualine_b = { "filename", "branch", "diff", "diagnostics" },
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = { "filetype", "progress" },
+					lualine_z = { { "location", separator = { left = "", right = "" } } },
+				},
+			})
 		end,
 	},
 }
